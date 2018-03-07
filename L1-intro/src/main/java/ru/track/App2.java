db0d0775acc0c6da9b1f1ec73045bfbfca858c37
foxmi-MAC <foxmiti@MBP-foxmi-MAC-2.lan>
2018-03-07 16:06:33 +0300

    Http request

--- L1-intro/src/main/java/ru/track/App2.java
+++ L1-intro/src/main/java/ru/track/App2.java
@@ -1,12 +1,32 @@
 package ru.track;
 
-
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
 import org.apache.commons.lang3.StringUtils;
 
 public class App2 {
+        public static final String URL = "http://guarded-mesa-31536.herokuapp.com/track";
+        public static final String FIELD_NAME = "name";
+        public static final String FIELD_GITHUB = "github";
+        public static final String FIELD_EMAIL = "email";
 
-    public static void main(String[] args) {
-        String str = StringUtils.capitalize(args[0]);
-        System.out.println(str);
-    }
+        public static void main (String[]args) throws Exception {
+            // 1) Use Unirest.post()
+            // 2) Get response .asJson()
+            // 3) Get json body and JsonObject
+            // 4) Get field "success" from JsonObject
+            HttpResponse<JsonNode> jsonResponse = Unirest.post(URL)
+                    .header("accept", "application/json")
+                    .field(FIELD_NAME, "Mitrikas Anton")
+                    .field(FIELD_GITHUB, "github.com/foxmiti")
+                    .field(FIELD_EMAIL, "mitrikasanton@gmail.com")
+                    .asJson();
+            System.out.println(jsonResponse.getBody().toString());
+
+
+            boolean success = false;
+//        String str = StringUtils.capitalize(args[0]);
+//       System.out.println(str);
+        }
 }
